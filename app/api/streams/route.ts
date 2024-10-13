78282c168f52192821d01e78e47e8637772c72a7
khalid09dev <mdkhalidhossen@hotmail.com>
2024-10-13 10:19:35 +0600

    have done some change the schema.prisma, have added a callback in user signin route and update the regex of youtube url and used youtubesearchapi named package for getting youtube video details through id

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,7 +1,10 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-const Yt_REGEX = new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}$");
+//@ts-ignore
+import youtubesearchapi from 'youtube-search-api';
+
+const Yt_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -11,7 +14,7 @@ const CreateStreamSchema = z.object({
 export async function POST(req: NextRequest) {
     try {
         const data = CreateStreamSchema.parse(await req.json());
-        const isYt = Yt_REGEX.test(data.url);
+        const isYt = data.url.match(Yt_REGEX)
         if(!isYt) {
             return NextResponse.json({
                 message: "Wrong Url"
@@ -22,19 +25,46 @@ export async function POST(req: NextRequest) {
 
         const extractedId = data.url.split("?v=")[1];
         
-        prismaClient.stream.create({
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        console.log(res.title, res.thumbnail.thumbnails);
+        const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1);
+
+        const stream = await prismaClient.stream.create({
             data: {
                 userId: data.creatorId,
                 url: data.url,
                 extractedId,
-                type: "Youtube"
+                type: "Youtube",
+                title: res.title ?? "",
+                smallImg: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "",
+                bigImg: thumbnails[thumbnails.length - 1].url ?? ""
             }
         })
+
+        return NextResponse.json({
+            message: "Stream added successfully",
+            id: stream.id,
+        })
     } catch (error) {
+        console.log(error);
         return NextResponse.json({
             message: "Error while adding a stream",
         }, {
             status: 411,
         })
     }
+}
+
+export async function GET(req: NextRequest) {
+    const creatorId = req.nextUrl.searchParams.get("creatorId");
+    const streams = await prismaClient.stream.findMany({
+        where: {
+            userId: creatorId ?? ""
+        }
+    })
+
+    return NextResponse.json({
+        streams,
+    })
 }
